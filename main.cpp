e8154ef4a07e94b1af884a5e93374053adab3461
Martin Neumann <neumann@insystems.de>
2019-06-26 00:54:52 +0200

    added omp to blur, not perfect

--- main.cpp
+++ main.cpp
@@ -45,7 +45,6 @@ void convertToYCbCr_omp(cv::Mat image) {
     std::cout << "Number of processors: " << omp_get_num_procs() << std::endl;
     omp_set_num_threads(omp_get_num_procs());
         int i, j;
-        cv::Mat tmpData;
         #pragma omp parallel for shared(image) private(i,j)
         for (i = 0; i <= image.cols; i++) {
             // std::cout << "This is thread " << omp_get_thread_num() << 
@@ -71,10 +70,10 @@ void convertToYCbCr_omp(cv::Mat image) {
     std::cout << "Converting finished using OpenMP." << std::endl;
     return;
 }
-
-void applyGaussianBlur(cv::Mat image) {
+cv::Mat applyGaussianBlur(cv::Mat image) {
     std::cout << "Applying Gaussian Blur on image." << std::endl;
     std::cout << "Creating kernel." << std::endl;
+    cv::Mat returnImage = image;
 
 
     double sigma = 1;
@@ -147,14 +146,101 @@ void applyGaussianBlur(cv::Mat image) {
                 // std::cout << std::endl;
             }
 
-            image.at<cv::Vec3b>(j, i)[0] = int(val1);
-            image.at<cv::Vec3b>(j, i)[1] = int(val2);
-            image.at<cv::Vec3b>(j, i)[2] = int(val3);
+            returnImage.at<cv::Vec3b>(j, i)[0] = int(val1);
+            returnImage.at<cv::Vec3b>(j, i)[1] = int(val2);
+            returnImage.at<cv::Vec3b>(j, i)[2] = int(val3);
             // std::cout << "Finished! " << val1 << " " << val2 << " " << val3 << " | " ;
             val1 = val2 = val3 = 0;
         }
     }
-    return;
+    return returnImage;
+}
+
+cv::Mat applyGaussianBlur_omp(cv::Mat image) {
+    std::cout << "Applying Gaussian Blur on image." << std::endl;
+    std::cout << "Creating kernel." << std::endl;
+    cv::Mat returnImage = image;
+
+
+    double sigma = 1;
+    int W = 5;
+    double kernel[W][W];
+    double mean = W/3;
+    double sum = 0.0; // For accumulating the kernel values
+    for (int x = 0; x < W; ++x)
+        for (int y = 0; y < W; ++y) {
+            kernel[x][y] = exp( -0.5 * (pow((x-mean)/sigma, 2.0) + pow((y-mean)/sigma,2.0)) )
+                / (2 * M_PI * sigma * sigma);
+
+            // Accumulate the kernel values
+            sum += kernel[x][y];
+        }
+
+    // Normalize the kernel
+    for (int x = 0; x < W; ++x)
+        for (int y = 0; y < W; ++y)
+            kernel[x][y] /= sum;
+    std::cout << "Kernel is: " << std::endl;
+    for (int a = 0; a < W; a++) {
+        for (int b = 0; b < W; b++) {
+            std::cout << kernel[a][b] << " ";
+        }
+        std::cout << std::endl;
+    }
+
+
+
+    std::cout << "Applying kernel." << std::endl;
+    int i, j;
+    int radius = W;
+    double val1 = 0, val2 = 0, val3 = 0;
+
+    // 1) loop over all pixels
+    #pragma omp parallel for shared(image,returnImage) private(i,j,val1,val2,val3)
+    for (i = 0; i < image.cols; i++) {
+        // std::cout << std::endl << "Column " << i << std::endl;
+        for (j = 0; j < image.rows; j++) {
+            // std::cout << j << ", ";
+
+            // 2) loop over kernel
+            int iy;
+            int ix;
+            // std::cout << "New kernel loop for pixel " << j << "," << i << std::endl;
+
+            for (iy = 0; iy < radius; iy++) {
+                // looping over y value of kernel
+                for (ix = 0; ix < radius; ix++) {
+                    // looping over x value of kernel
+
+                    // values for multiplication 
+                    int x = std::min(image.cols, std::max(0, i-ix));
+                    int y = std::min(image.rows, std::max(0, j-iy));
+
+
+                    // std::cout << "Applying at positions: " << y << "," << x;
+                    /*std::cout << ". Value: " << image.at<cv::Vec3b>(y,x) << " * "
+                      << kernel[ix][iy] << std::endl;
+
+                      char str[20];
+                      std::scanf("%s", str);
+
+*/
+                    val1 += (image.at<cv::Vec3b>(y, x)[0] * kernel[ix][iy]);
+                    val2 += (image.at<cv::Vec3b>(y, x)[1] * kernel[ix][iy]);
+                    val3 += (image.at<cv::Vec3b>(y, x)[2] * kernel[ix][iy]);
+                    //  std::cout << val1 << ", " << val2 << ", " << val3 << std::endl;
+                } 
+                // std::cout << std::endl;
+            }
+
+            returnImage.at<cv::Vec3b>(j, i)[0] = int(val1);
+            returnImage.at<cv::Vec3b>(j, i)[1] = int(val2);
+            returnImage.at<cv::Vec3b>(j, i)[2] = int(val3);
+            // std::cout << "Finished! " << val1 << " " << val2 << " " << val3 << " | " ;
+            val1 = val2 = val3 = 0;
+        }
+    }
+    return returnImage;
 }
 
 int main(int argc, char *argv[]) {
@@ -195,9 +281,9 @@ int main(int argc, char *argv[]) {
      */
 
     // loop over array
-    convertToYCbCr_omp(image);
-    cv::imwrite("converted.png", image);
-    //applyGaussianBlur(image);
+    // convertToYCbCr_omp(image);
+    // cv::imwrite("converted.png", image);
+    auto gaussianImage = applyGaussianBlur(image);
     //cv::imwrite("gaussian.png", image);
     // auto gaussianBlurComparison = applyGaussianBlur(image);
     // cv::imwrite("gaussiancomparison.png", gaussianBlurComparison);
@@ -211,7 +297,7 @@ int main(int argc, char *argv[]) {
         << "Size: " << newImage.size() << std::endl
         << "Type: " << newImage.type() << std::endl;
     std::cout << "type of comparison image is: " << typeid(newImage).name() << std::endl;
-    imshow( "Display window", image);                   // Show our image inside it.
+    imshow( "Display window", gaussianImage);                   // Show our image inside it.
     cv::waitKey(0);
     return  0;
 }
